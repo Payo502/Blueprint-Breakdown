cdd40aa5d72a26f4edf4a0e264aec782cafb27fa
Jan Benko <jan.benko777@gmail.com>
2023-05-10 09:55:12 +0200

    Necessary commit for pull

--- GXPEngine2023/GXPEngine/MyGame.cs
+++ GXPEngine2023/GXPEngine/MyGame.cs
@@ -6,10 +6,10 @@ public class MyGame : Game {
 	Player player;
 	public MyGame() : base(800, 600, false)    
 	{
-		player = new Player(new Vec2(200,200),30);
+		player = new Player(new Vec2(200, 200), 30);
 		AddChild(player);
 
-        AddChild(new Line(new Vec2(0, 500), new Vec2(800, 500)));
+		AddChild(new Line(new Vec2(0, 500), new Vec2(800, 500)));
 		AddChild(new Line(new Vec2(100, 0), new Vec2(100, 800)));
 		AddChild(new Line(new Vec2(700, 0), new Vec2(700, 800)));
 		AddChild(new Line(new Vec2(0, 100), new Vec2(800, 100)));
