a350c56fc3f29b7bbabce4bc1bb9ea05dae80ee3
Jan Benko <jan.benko777@gmail.com>
2023-05-12 11:56:02 +0200

    Button

--- GXPEngine2023/GXPEngine/Button.cs
+++ GXPEngine2023/GXPEngine/Button.cs
@@ -6,12 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-public class Button : GameObject
-{
-    public Button() : base()
-    {
-
-    }
-}
+//public class Button : GameObject
+//{
+//    Vec2 position;
+//    public Button(Vec2 pPosition,) : base()
+//    {
+//        position = pPosition;
+//    }
+//}
 
 
