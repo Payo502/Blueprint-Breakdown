7acfe9c9075136ffc6970ff754865b363cf33c89
Payo502 <patriciogutierrez236@gmail.com>
2023-05-16 15:33:06 +0200

    changes

--- GXPEngine2023/GXPEngine/Fan.cs
+++ GXPEngine2023/GXPEngine/Fan.cs
@@ -28,7 +28,7 @@ public class Fan : EasyDraw
 
     MapObject mapObject;
 
-    public Fan(Vec2 pStart, Vec2 pEnd, Vec2 pScale, int airLength = 500) : base(1000, 1000, false)
+    public Fan(Vec2 pStart, Vec2 pEnd, Vec2 pScale, int airLength = 500, float airStrength = 1) : base(1000, 1000, false)
     {
         Clear(255);
         start = pStart;
